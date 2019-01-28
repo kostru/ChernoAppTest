9d51efb5f9878b584aa333dcd93f940b5922383e
VOIDVII <sinann16188@gmail.com>
2019-01-28 17:03:15 +0100

    basic UI

--- app/src/main/java/com/chernoinc/chernoapp/Journals.java
+++ app/src/main/java/com/chernoinc/chernoapp/Journals.java
@@ -2,14 +2,16 @@ package com.chernoinc.chernoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class Journals extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_journals);
 
+        ListView listv_journals = (ListView) findViewById(R.id.listv_journals);
 
     }
 }
